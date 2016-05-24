41c4ac3f172aa50149b505bc0a3b45cb497620a1
throwawaylbeatty <liam.beatty.fhvs@statefarm.com>
2016-05-24 11:18:56 -0700

    Created skeleton class
    Ensures skel dir and .bashrc file sourced from /modules/skeleton/.bashrc

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,11 @@
+class skeleton {
+  file { 'skelDir':
+    ensure => directory,
+    path => '/etc/skel',
+  }
+  file { 'skel':
+    ensure  => present,
+    path    => '/etc/skel/.bashrc',
+    source  => puppet:///modules/skeleton/.bashrc,
+  }
+}
