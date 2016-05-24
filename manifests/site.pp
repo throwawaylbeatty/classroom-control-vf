f2eeb5802962e6c5661087c401489fba8a8ed711
throwawaylbeatty <liam.beatty.fhvs@statefarm.com>
2016-05-24 09:35:26 -0700

    fixed stuff
    ssh -> users

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,9 +38,9 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-#node throwawaylbeatty.puppetlabs.vm {
-#  include ssh
-#}
+node throwawaylbeatty.puppetlabs.vm {
+  include users
+}
 
 node default {
   # This is where you can declare classes for all nodes.
