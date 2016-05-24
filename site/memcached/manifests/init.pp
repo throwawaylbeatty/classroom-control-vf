aab63b9d347876755a7c3af3ab3dbb22973d8df1
throwawaylbeatty <liam.beatty.fhvs@statefarm.com>
2016-05-24 12:17:33 -0700

    added resource relationships

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -3,17 +3,19 @@ class memcached {
     name    => 'memcached',
     ensure  => present,
   }
-  
   file { 'configfile':
     path    => '/etc/sysconfig/memcached',
-    ensure  => present,
+    ensure  => file,
+    group   => 'root',
+    mode    => '0644',
+    owner   => 'root',
     source  => 'puppet:///modules/memcached/memcached_config',
+    require => Package['Installmemcached'],
+    notify  => Service['startMemcached'],
   }
-  
   service { 'startMemcached':
     ensure  => running,
     enable  => true,
+    require => File['configfile']
   }
-  
-
 }
