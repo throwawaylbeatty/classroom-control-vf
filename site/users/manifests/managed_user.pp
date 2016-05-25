754ef517cbfd501700994fab4824cdb42206f358
throwawaylbeatty <liam.beatty.fhvs@statefarm.com>
2016-05-25 12:29:39 -0700

    Update managed_user.pp
    asdf

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,10 +1,12 @@
-define users::managed_user ( $group = $title, ) {
-  users { $title:
+define users::managed_user (
+  $group = $title,
+  ) {
+  user { $title:
     ensure => present,
   }
-  file { "/home/${title}" :
-    ensure  => directory,
-    owner   => $title,
-    group   => $group,
+  file { "/home/${title}":
+    ensure => directory,
+    owner => $title,
+    group => $group,
   }
 }
