3d00ae992eff60ebce45e37c5c11bb1dc8532407
throwawaylbeatty <liam.beatty.fhvs@statefarm.com>
2016-05-25 07:25:28 -0700

    added rpm install

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,9 +1,18 @@
 class nginx {
 # ensures the nginx webserver package is installed
-  package { 'install nginx' :
-    name    => 'nginx',
-    ensure  => present,
+  file { 'nginx rpm' :
+    ensure   => file,
+    path     => '/opt/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
+    source   => 'puppet:///modules/nginx/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
   }
+
+  package { 'nginx' :
+    ensure   => '1.6.2-1.el7.centos.ngx',
+    source   => '/opt/nginx-1.6.2-1.el7.centos.ngx.x86_64.rpm',
+    provider => rpm,
+    require  => File['nginx rpm'],
+  }
+  
 # ensures that a document root directory is present to store web pages.
   file { 'nginx dir' :
     path    => '/var/www',
