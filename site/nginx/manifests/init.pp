b0802e683685d0fd9711c906374930a501b5c175
throwawaylbeatty <liam.beatty.fhvs@statefarm.com>
2016-05-25 07:36:35 -0700

    added yum repos

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,4 +1,51 @@
 class nginx {
+# add yum repos to download the packages
+  yumrepo { 'base':
+    ensure              => 'present',
+    descr               => 'CentOS-$releasever - Base',
+    enabled             => '1',
+    gpgcheck            => '1',
+    gpgkey              => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
+    mirrorlist          => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os&infra=$infra',
+    priority            => '99',
+    skip_if_unavailable => '1',
+    before     => [ Package['nginx'], Package['openssl-libs'] ],
+  }
+  
+  yumrepo { 'updates':
+    ensure              => 'present',
+    descr               => 'CentOS-$releasever - Updates',
+    enabled             => '1',
+    gpgcheck            => '1',
+    gpgkey              => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
+    mirrorlist          => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra',
+    priority            => '99',
+    skip_if_unavailable => '1',
+    before     => [ Package['nginx'], Package['openssl-libs'] ],
+  }
+  
+  yumrepo { 'extras':
+    ensure              => 'present',
+    descr               => 'CentOS-$releasever - Extras',
+    enabled             => '1',
+    gpgcheck            => '1',
+    gpgkey              => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
+    mirrorlist          => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras&infra=$infra',
+    priority            => '99',
+    skip_if_unavailable => '1',
+    before     => [ Package['nginx'], Package['openssl-libs'] ],
+  }
+  
+  yumrepo { 'centosplus':
+    ensure     => 'present',
+    descr      => 'CentOS-$releasever - Plus',
+    enabled    => '1',
+    gpgcheck   => '1',
+    gpgkey     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
+    mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=centosplus&infra=$infra',
+    before     => [ Package['nginx'], Package['openssl-libs'] ],
+  }
+  
 # ensures the nginx webserver package is installed
   package { [ 'openssl', 'openssl-libs' ] :
     ensure => '1.0.1e-51.el7_2.5',
