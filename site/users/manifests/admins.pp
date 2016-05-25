bf8337deb48ee13e79288ede5621138fe3432226
throwawaylbeatty <liam.beatty.fhvs@statefarm.com>
2016-05-25 12:29:07 -0700

    Update admins.pp
    asdf

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,12 +1,13 @@
 class users::admins {
-  users::managed_user{'joe' : }
-  users::managed_user{'alice' :
+  users::managed_user { 'joe': }
+  users::managed_user { 'alice':
     group => 'staff',
   }
-  users::managed_user{'aaron' :
+  users::managed_user { 'aaron':
     group => 'staff',
   }
-  group { 'staff' :
-    ensure  => present,
+  group { 'staff':
+    ensure => present,
   }
 }
+
